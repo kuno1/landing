f979db729a500ed044e756a97eb6ac968933725a
fumikito <guy@hametuha.com>
2017-09-13 17:58:02 +0900

    Final release

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-var gulp        = require('gulp'),
-    fs          = require('fs'),
-    $           = require('gulp-load-plugins')(),
-    pngquant    = require('imagemin-pngquant'),
+var gulp = require('gulp'),
+    fs = require('fs'),
+    $ = require('gulp-load-plugins')(),
+    pngquant = require('imagemin-pngquant'),
     eventStream = require('event-stream'),
     browserSync = require('browser-sync').create();
 
@@ -40,44 +40,50 @@ var destDir = {
 // Sass
 gulp.task('sass', function () {
 
-  return gulp.src(srcDir.scss)
-    .pipe($.plumber({
-        errorHandler: $.notify.onError('<%= error.message %>')
-    }))
-    .pipe($.sassBulkImport())
-    .pipe($.sourcemaps.init())
-    .pipe($.sass({
-      errLogToConsole: true,
-      outputStyle    : 'compressed',
-      sourceComments : 'normal',
-      sourcemap      : true,
-      includePaths   : includesPaths
-    }))
-    .pipe($.sourcemaps.write('./map'))
-    .pipe(gulp.dest(destDir.scss));
+    return gulp.src(srcDir.scss)
+        .pipe($.plumber({
+            errorHandler: $.notify.onError('<%= error.message %>')
+        }))
+        .pipe($.sassBulkImport())
+        .pipe($.sourcemaps.init())
+        .pipe($.sass({
+            errLogToConsole: true,
+            outputStyle: 'compressed',
+            sourceComments: 'normal',
+            sourcemap: true,
+            includePaths: includesPaths
+        }))
+        .pipe($.sourcemaps.write({includeContent: false}))
+        .pipe($.sourcemaps.init({loadMaps: true}))
+        .pipe($.autoprefixer({
+            browsers: ['last 2 version', 'iOS >= 8.1', 'Android >= 4.4'],
+            cascade: false
+        }))
+        .pipe($.sourcemaps.write('./map'))
+        .pipe(gulp.dest(destDir.scss));
 });
 
 
 // Minify All
 gulp.task('jsconcat', function () {
-  return gulp.src(srcDir.js)
-    .pipe($.sourcemaps.init({
-      loadMaps: true
-    }))
-    .pipe($.include())
-    .pipe($.uglify())
-    .on('error', $.util.log)
-    .pipe($.sourcemaps.write('./map'))
-    .pipe(gulp.dest(destDir.js));
+    return gulp.src(srcDir.js)
+        .pipe($.sourcemaps.init({
+            loadMaps: true
+        }))
+        .pipe($.include())
+        .pipe($.uglify())
+        .on('error', $.util.log)
+        .pipe($.sourcemaps.write('./map'))
+        .pipe(gulp.dest(destDir.js));
 });
 
 
 // JS Hint
 gulp.task('jshint', function () {
-  return gulp.src(srcDir.jsHint)
-    .pipe($.plumber())
-    .pipe($.jshint(srcDir.jshintrc))
-    .pipe($.jshint.reporter('jshint-stylish'));
+    return gulp.src(srcDir.jsHint)
+        .pipe($.plumber())
+        .pipe($.jshint(srcDir.jshintrc))
+        .pipe($.jshint.reporter('jshint-stylish'));
 });
 
 // JS task
@@ -86,35 +92,35 @@ gulp.task('js', ['jshint', 'jsconcat']);
 
 // Build Libraries.
 gulp.task('copylib', function () {
-  // pass gulp tasks to event stream.
-  // return eventStream.merge(
-  // );
+    // pass gulp tasks to event stream.
+    // return eventStream.merge(
+    // );
 });
 
 // Image min
 gulp.task('imagemin', function () {
-  return gulp.src(srcDir.img)
-    .pipe($.imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
-      use        : [pngquant()]
-    }))
-    .pipe(gulp.dest(destDir.img));
+    return gulp.src(srcDir.img)
+        .pipe($.imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest(destDir.img));
 });
 
 
 // watch
 gulp.task('watch', function () {
-  // Make SASS
-  gulp.watch(srcDir.scss, ['sass']);
-  // Uglify all
-  gulp.watch(srcDir.jsHint, ['js']);
-  // Minify Image
-  gulp.watch(srcDir.img, ['imagemin']);
+    // Make SASS
+    gulp.watch(srcDir.scss, ['sass']);
+    // Uglify all
+    gulp.watch(srcDir.jsHint, ['js']);
+    // Minify Image
+    gulp.watch(srcDir.img, ['imagemin']);
 });
 
 // Browser Sync
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browserSync.init({
         server: {
             baseDir: "./docs"
@@ -123,7 +129,7 @@ gulp.task('browser-sync', function() {
 });
 
 //
-gulp.task('reload', function() {
+gulp.task('reload', function () {
     return gulp.watch([
         './docs/**/*'
     ]).on('change', browserSync.reload);
